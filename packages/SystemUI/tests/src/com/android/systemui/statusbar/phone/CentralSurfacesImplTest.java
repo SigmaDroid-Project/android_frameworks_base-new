5180166606fca79144e27e89c12979ece8a7cb79
Co-authored-by: beanstown106 <nbenis106@gmail.com>
2022-11-20 21:19:00 +0530

    SystemUI: Max Lockscreen Notification count [1/2]
    [SuperDroidBond: adapt for Android 13 & Android 14]
    [Albinoman887: Adapt for Android 15]
    
    Co-authored-by: beanstown106 <nbenis106@gmail.com>
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/CentralSurfacesImplTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/CentralSurfacesImplTest.java
@@ -250,6 +250,7 @@ public class CentralSurfacesImplTest extends SysuiTestCase {
     @Mock private HeadsUpManager mHeadsUpManager;
     @Mock private NotificationPanelViewController mNotificationPanelViewController;
     @Mock private ShadeLogger mShadeLogger;
+    @Mock private NotificationPanelViewController mNewNotificationPanelViewController;
     @Mock private NotificationPanelView mNotificationPanelView;
     @Mock private QuickSettingsController mQuickSettingsController;
     @Mock private IStatusBarService mBarService;
@@ -525,6 +526,7 @@ public class CentralSurfacesImplTest extends SysuiTestCase {
                 new JavaAdapter(mTestScope),
                 mUiBgExecutor,
                 mNotificationPanelViewController,
+                mNewNotificationPanelViewController,
                 mNotificationMediaManager,
                 mLockscreenUserManager,
                 mRemoteInputManager,
@@ -583,6 +585,7 @@ public class CentralSurfacesImplTest extends SysuiTestCase {
                 mLockscreenTransitionController,
                 mFeatureFlags,
                 mKeyguardUnlockAnimationController,
+                new Handler(TestableLooper.get(this).getLooper()),
                 mMainExecutor,
                 mMessageRouter,
                 mWallpaperManager,
