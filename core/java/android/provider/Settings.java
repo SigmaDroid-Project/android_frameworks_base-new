dbf52e873fb767b9b51f8f652ce03a444bdfe5a0
elpaablo <donopwhile1@gmail.com>
2023-08-23 20:19:24 +0100

    Add Ambient Customizations [1/2]
    This adds the option to add custom text and a custom image on the ambient screen. This option is still in development, so be patient, the bugs that arise will be resolved.
    Based on OmniROM Edge Lighting and Custom Headers.
    
    [SuperDroidBond: adapt for R + A13 & put it behind notification lights]
    
    Good Work by @/TikkiX2
    
    Change-Id: I8bbda7e908ef4bae892c59fdbcf6032a799b3f5b
    
    fixup! Ambient Customizations
    
    SDB:
    1. Fix Text Animation
    2. Fix Pulse Lights not visible if Ambient Image activated
    3. Catch NPE in KeyguardVisibilityHelper
    4. Misc. fixes for Ambient Text & Images
    
    minaripenguin:
    1. Cleanup unused resources
    2. make ambient text follow clock font
    3. Minor code refactoring
    
    [elpaablo]:
    * Port to Android 14
    
    Change-Id: Icbc66effd362d5e7d4e77d977960778bfc19dc7a
    
    Co-authored-by: TikkiTikki <radstylemusic@gmail.com>
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: elpaablo <donopwhile1@gmail.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7299,6 +7299,60 @@ public final class Settings {
          */
         public static final String SEEK_BAR_BATTERY_CHARGE_LEVEL_SOUND = "battery_level_charge_seek_bar";
 
+       /**
+        * Ambient Customization
+        * @hide
+        */
+        public static final String AMBIENT_TEXT = "ambient_text";
+
+        /**
+         * Ambient text string
+         * @hide
+         */
+        public static final String AMBIENT_TEXT_STRING = "ambient_text_string";
+
+        /**
+         * Ambient text animation
+         * @hide
+         */
+        public static final String AMBIENT_TEXT_ANIMATION = "ambient_text_animation";
+
+        /**
+         * Ambient text size
+         * @hide
+         */
+        public static final String AMBIENT_TEXT_SIZE = "ambient_text_size";
+
+        /**
+         * Ambient text alignment
+         * @hide
+         */
+        public static final String AMBIENT_TEXT_ALIGNMENT = "ambient_text_alignment";
+
+        /**
+         * Ambient text color
+         * @hide
+         */
+        public static final String AMBIENT_TEXT_COLOR = "ambient_text_color";
+
+        /**
+         * Ambient text type color
+         * @hide
+         */
+        public static final String AMBIENT_TEXT_TYPE_COLOR = "ambient_text_type_color";
+
+        /**
+        * Ambient Customization
+        * @hide
+        */
+        public static final String AMBIENT_IMAGE = "ambient_image";
+
+        /**
+         * Ambient Custom Image
+         * @hide
+         */
+        public static final String AMBIENT_CUSTOM_IMAGE = "ambient_custom_image";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -7438,6 +7492,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(CUSTOM_KEYGUARD_BATTERY_BAR_COLOR_SOURCE);
             PRIVATE_SETTINGS.add(CUSTOM_KEYGUARD_BATTERY_BAR_CUSTOM_COLOR);
             PRIVATE_SETTINGS.add(LOCKSCREEN_BATTERY_INFO_TEMP_UNIT);
+            PRIVATE_SETTINGS.add(AMBIENT_TEXT_COLOR);
+            PRIVATE_SETTINGS.add(AMBIENT_TEXT_TYPE_COLOR);
         }
 
         /**
