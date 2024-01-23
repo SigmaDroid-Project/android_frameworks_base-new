85af0c8a22ae3c4db309027ac39b59540057645f
maxwen <max.weninger@gmail.com>
2024-01-23 00:59:31 -0700

    SystemUI: Introduce keyguard battery bar settings [1/3]. Squashed-in: * SystemUI: keyguard battery bar in aod on charging [1/2]   or always with an switch (maxwen) [micky387] Remove tuner dependency and add settings for our OmniGears
    [spezi77] Updated for A13 & A14
    * SystemUI: Also show the battery bar if the battery is not charging.
      Only show it while in doze mode (Ambient/AoD) (spezi77)
    
    [Albinoman887] Fix USB plug/unplug on AOD when battery bar setting is disabled
    * This check was added after the following commit that intruduces the Keyguard Battery Bar
      Setting was authored. Simply bring back the check and bail out of the function if it isn't
      visible.
    
    Co-authored-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7229,6 +7229,16 @@ public final class Settings {
         public static final String VOWIFI_ICON_STYLE = "vowifi_icon_style";
 
         /**
+         * @hide
+         */
+        public static final String CUSTOM_KEYGUARD_SHOW_BATTERY_BAR = "sysui_keyguard_show_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String CUSTOM_KEYGUARD_SHOW_BATTERY_BAR_ALWAYS = "sysui_keyguard_show_battery_bar_always";
+
+        /**
          * Clock font size
          * @hide
          */
@@ -7464,6 +7474,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DEFAULT_DEVICE_FONT_SCALE);
             PRIVATE_SETTINGS.add(AMBIENT_TEXT_COLOR);
             PRIVATE_SETTINGS.add(AMBIENT_TEXT_TYPE_COLOR);
+            PRIVATE_SETTINGS.add(CUSTOM_KEYGUARD_SHOW_BATTERY_BAR);
+            PRIVATE_SETTINGS.add(CUSTOM_KEYGUARD_SHOW_BATTERY_BAR_ALWAYS);
         }
 
         /**
