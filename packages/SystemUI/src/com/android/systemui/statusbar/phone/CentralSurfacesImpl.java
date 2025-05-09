a3a97786f85ecc48f717f2fce0f69e58b02345a9
Matt Filetto <matt.filetto@gmail.com>
2025-05-09 02:48:49 -0700

    add missing import
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -55,6 +55,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.graphics.Point;
 import android.hardware.devicestate.DeviceStateManager;
 import android.hardware.display.DisplayManager;
